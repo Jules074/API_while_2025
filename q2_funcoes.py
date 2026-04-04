c10ef74fefcc68e4c853642c9480ccd92b64a165
Juliana <juferrazju2007@gmail.com>
2026-04-04 11:04:46 -0300

    max_num() e min_max()

--- q2_funcoes.py
+++ q2_funcoes.py
@@ -5,31 +5,50 @@
 #e. Receber número inteiro numa faixa min X e max Y
 
 def main():
-    num_int = min_num()
+    num_int = min_max('insira um limite minimo:\n',\
+                      'insira um limite maximo:\n',\
+                      'insira um valor inteiro:\n')
     print(num_int)
 
 
-def inteiro():
+def inteiro(label: str):
     while True:
         try:   
-            n = int(input())
+            n = int(input(label))
             return n
         except:
             print('o numero não é inteiro, tente novamente')
 
-def int_positivo():
-    n = inteiro()
+def int_positivo(label: str):
+    n = inteiro(label)
     while 0 > n:
         print('numero negativo, tente novamente')
-        n = inteiro()
+        n = inteiro(label)
     return n
 
-def min_num():
-    limite = inteiro('limite: ')
-    n = inteiro()
+def min_num(label: str, label3: str):
+    limite = inteiro(label)
+    n = inteiro(label3)
     while n < limite:
         print('numero fora do limite minimo...')
+        n = inteiro(label3)
     return n
 
+def max_num(label1: str, label3: str):
+    limite = inteiro(label1)
+    n = inteiro(label3)
+    while n > limite:
+        print('numero fora do limite maximo...')
+        n = inteiro(label3)
+    return n
+
+def min_max(label1: str, label2: str, label3: str):
+    menor = min_num(label1, label3)
+    maior = max_num(label2, label3)
+    n = inteiro(label3)
+    while menor < n < maior:
+        print('valor não está entre os limites')
+        n = inteiro(label3)
+    return n
     
 main()
