64959f238b0d71c61cc8fafc791bacbdc4ebfeea
Jules074 <juferrazju2007@gmail.com>
2026-04-04 12:25:05 -0300

    menor = inteiro(); maior = inteiro()

--- q2_funcoes.py
+++ q2_funcoes.py
@@ -26,16 +26,16 @@ def int_positivo(label: str):
         n = inteiro(label)
     return n
 
-def min_num(label: str, label3: str):
-    limite = inteiro(label)
+def min_num(label1: str, label3: str):
+    limite = inteiro(label1)
     n = inteiro(label3)
     while n < limite:
         print('numero fora do limite minimo...')
         n = inteiro(label3)
     return n
 
-def max_num(label1: str, label3: str):
-    limite = inteiro(label1)
+def max_num(label2: str, label3: str):
+    limite = inteiro(label2)
     n = inteiro(label3)
     while n > limite:
         print('numero fora do limite maximo...')
@@ -43,10 +43,10 @@ def max_num(label1: str, label3: str):
     return n
 
 def min_max(label1: str, label2: str, label3: str):
-    menor = min_num(label1, label3)
-    maior = max_num(label2, label3)
+    menor = inteiro(label1)
+    maior = inteiro(label2)
     n = inteiro(label3)
-    while menor < n < maior:
+    while not(menor < n < maior):
         print('valor não está entre os limites')
         n = inteiro(label3)
     return n
